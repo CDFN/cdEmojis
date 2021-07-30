069f2655db53586ed5e56e7124984037f6eaede8
CDFN <codefun@protonmail.com>
2021-07-30 23:20:47 +0200

    Add some comments

--- resourcepack-generator/main.py
+++ resourcepack-generator/main.py
@@ -8,14 +8,18 @@ resourcepackPath = "resourcepack/assets/cdemojis/"
 fontDirPath = resourcepackPath + "font/"
 fontTexturesDirPath = resourcepackPath + "textures/font/"
 
+# Delete all directories so we have fresh start
 shutil.rmtree(fontDirPath, ignore_errors=True)
 shutil.rmtree(fontTexturesDirPath, ignore_errors=True)
 
+# Recreate empty directories
 os.makedirs(fontDirPath)
 os.makedirs(fontTexturesDirPath)
 
+# Open file containing emoji mappings
 with open('allmoji.json') as emojiFile:
     emojis = json.load(emojiFile)
+    # Initialize Minecraft's font object
     lang_file = {
         'providers': []
     }
@@ -25,6 +29,7 @@ with open('allmoji.json') as emojiFile:
         filePath = emojisPath + rawName
         if not path.exists(filePath):
             continue
+        # Add emoji to font object
         lang_file['providers'].append(
                 {
                     "type": "bitmap",
@@ -34,7 +39,9 @@ with open('allmoji.json') as emojiFile:
                               emojis[emoji]['codepoints']]
                 }
         )
+        # Copy emoji PNG into resourcepack folder
         shutil.copyfile(filePath, fontTexturesDirPath + rawName)
 
+    # Dump font object into file as json
     with open(fontDirPath + 'default.json', 'w') as outfile:
         json.dump(lang_file, outfile, indent=2, ensure_ascii=False)
