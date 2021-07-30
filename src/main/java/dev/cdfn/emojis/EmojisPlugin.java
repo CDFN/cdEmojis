f1957575f6d300653fe69ea321115c133bbfa09f
CDFN <codefun@protonmail.com>
2021-07-30 22:10:33 +0200

    Initial commit

---
+++ src/main/java/dev/cdfn/emojis/EmojisPlugin.java
@@ -0,0 +1,14 @@
+package dev.cdfn.emojis;
+
+import kr.entree.spigradle.annotations.SpigotPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SpigotPlugin
+public final class EmojisPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+}
